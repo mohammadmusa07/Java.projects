bfe6e6bd8f7131d5329ecc0d8424d28a9fb59292
Mohammad Musa <musa.mohammad.dev@gmail.com>
2026-08-06 22:52:03 +0530

    updated

--- loops.java
+++ loops.java
@@ -1,7 +1,21 @@
+import java.util.*;
 public class loops {
             public static void main(String[] args) {
-for(int i = 0; i < 10;i++){
-    System.out.print(i+" ");
+// for(int i = 0; i < 10;i++){
+//     System.out.print(i+" ");
+// 
+// int i = 0;
+// while(i < 20){
+//     System.out.println(i);
+//     i++;
+Scanner sc = new Scanner(System.in);
+int n = sc.nextInt();
+int sum = 0;
+for(int i = 0; i <= n; i++){
+    sum = sum + i;
 }
+System.out.println(sum);
+
+
+        }
     }
-}
