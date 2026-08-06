b954432123998335e670af05eb9f771f90676bed
Mohammad Musa <musa.mohammad.dev@gmail.com>
2026-08-06 12:57:02 +0530

    Java Practice

---
+++ radiustable.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class radiustable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // int r = sc.nextInt();
+        // double area = Math.PI * r * r;
+        // System.out.println(area);
+        // int a = 2;
+        // double peri = Math.PI * r * a;
+
+        // System.out.println(peri);
+int t = sc.nextInt();
+// System.out.println(t * 1);
+//  System.out.println(t * 2);
+//  System.out.println(t * 3);
+//  System.out.println(t * 4);
+//  System.out.println(t * 5);
+//  System.out.println(t * 6);
+//  System.out.println(t * 7);
+//  System.out.println(t * 8);
+// System.out.println(t * 9);
+// System.out.println(t * 10);
+System.out.println(t + " x 1 = " + t * 1);
+System.out.println(t + " x 2 = " + t * 2);
+System.out.println(t + " x 3 = " + t * 3);
+System.out.println(t + " x 4 = " + t * 4);
+System.out.println(t + " x 5 = " + t * 5);
+System.out.println(t + " x 6 = " + t * 6);
+System.out.println(t + " x 7 = " + t * 7);
+System.out.println(t + " x 8 = " + t * 8);
+System.out.println(t + " x 9 = " + t * 9);
+System.out.println(t + " x 10 = " + t * 10);
+
+
+    }
+}
---
+++ starpattern.java
@@ -0,0 +1,5 @@
+class starpattern {
+    public static void main(String[] args) {
+        System.out.println("*\n**\n***\n****");
+    }
+}
