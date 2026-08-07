dbfd0ffa58460835fef7b2570f7715f5a33cf805
Mohammad Musa <musa.mohammad.dev@gmail.com>
2026-08-07 22:01:31 +0530

    update

--- marksresult.java
+++ marksresult.java
@@ -1,38 +1,31 @@
 import java.util.*;
 public class marksresult {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 Scanner sc = new Scanner(System.in);
 int a;
-do{ System.out.println("1 or 0");
-a = sc.nextInt();
-if(a == 0){
-    System.out.println("program ends");
-} else if (a == 1){
-    System.out.println("enter marks");
-    int b = sc.nextInt();
-    if(b >= 90 && b < 101){
-        System.out.println("Excellent");
-     } else if(b >= 60){ 
-            System.out.println("good");
-} else if(b > 0){
-    System.out.println("bad");
-} else {
-    System.out.println("invalid");
-}
-} else { 
-    System.out.println("invalid");
-    }  
-} while (a != 0);
+do{ System.out.println("0 OR 1");
+    a = sc.nextInt();
+    if(a == 0){
+        System.out.println("end program");
+    } else if(a == 1){ System.out.println("ENTER MARKS");
+             int b = sc.nextInt();
+     if(b >= 90 && b < 101){
+                System.out.println("EXCELLENT");
+    } else if (b >= 60 && b < 101 ){
+        System.out.println("GOOD");
+    } else if(b >= 0 && b < 101){System.out.println("BAD");
+    }   else  {
+    System.out.println("INVALID");
     }
-}
-    
+    } else { 
+    System.out.println("INVALID");
+    }
+    } while (a != 0);
+        }
+    }
 
 
 
+            
         
     
-
-
-
-    
-
