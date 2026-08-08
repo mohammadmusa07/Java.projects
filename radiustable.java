fae44194d499ffc107c1f6b4c12b7f7d25bc390c
Mohammad Musa <musa.mohammad.dev@gmail.com>
2026-08-08 14:00:47 +0530

    update

--- radiustable.java
+++ radiustable.java
@@ -2,14 +2,14 @@ import java.util.*;
 public class radiustable {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // int r = sc.nextInt();
-        // double area = Math.PI * r * r;
-        // System.out.println(area);
-        // int a = 2;
-        // double peri = Math.PI * r * a;
+        int r = sc.nextInt();
+        double area = Math.PI * r * r;
+        System.out.println(area);
+        int a = 2;
+        double peri = Math.PI * r * a;
 
-        // System.out.println(peri);
-int t = sc.nextInt();
+        System.out.println(peri);
+// int t = sc.nextInt();
 // System.out.println(t * 1);
 //  System.out.println(t * 2);
 //  System.out.println(t * 3);
@@ -30,10 +30,10 @@ int t = sc.nextInt();
 // System.out.println(t + " x 8 = " + t * 8);
 // System.out.println(t + " x 9 = " + t * 9);
 // System.out.println(t + " x 10 = " + t * 10);
-for(int i = 1; i < 11; i++){
-    System.out.println(t + "x" + i + "=" + t*i);
+// for(int i = 1; i < 11; i++){
+//     System.out.println(t + "x" + i + "=" + t*i);
 }
 
 
     }
-}
+
