088bbe631a89827287b4041b6b0b890361908cd0
Mohammad Musa <musa.mohammad.dev@gmail.com>
2026-08-12 22:12:40 +0530

    updates

---
+++ variables.java
@@ -0,0 +1,9 @@
+public enum variables {
+    public  static void main(String[] args) {
+        int a = 10;
+        int b = 35;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+     
+}
