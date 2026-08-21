1c0150da265b8e9aecc949a02dc45c5856eb6caf
Mohammad Musa <musa.mohammad.dev@gmail.com>
2026-08-21 12:39:05 +0530

    updates

--- hollowbutterfly.java
+++ hollowbutterfly.java
@@ -4,6 +4,44 @@ public class hollowbutterfly {
     public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
             int n = sc.nextInt();
-            
+            for ( int i = 1; i <= n ; i++){
+                for ( int j = 1 ; j <= i ; j++){
+                    if( j == 1 || j== i ){
+                    System.out.print("*");
+                    } else { 
+                        System.out.print(" ");
+                    }
+            }
+            for( int spaces = 1 ; spaces <= 2 * (n - i ); spaces++){
+                System.out.print(" ");
+            }
+            for ( int j = 1 ; j <= i ; j++){
+ if( j == 1 || j== i ){
+                System.out.print("*");
+                    } else { 
+                        System.out.print(" ");
+                    }
     }
+    System.out.println();
+}
+for ( int i = n; i >= 1 ; i--){
+                for ( int j = 1 ; j <= i ; j++){
+ if( j == 1 || j== i ){ System.out.print("*");
+                } else { 
+                        System.out.print(" ");
+                    }
+            }
+            for( int spaces = 1 ; spaces <= 2 * (n - i ); spaces++){
+                System.out.print(" ");
+            }
+            for ( int j = 1 ; j <= i ; j++){
+ if( j == 1 || j== i ){
+                System.out.print("*");
+                    } else { 
+                        System.out.print(" ");
+                    }
+    }
+    System.out.println();
+}
+}
 }
