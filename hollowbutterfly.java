09e32e89878b1bec266b5a91f81478e258577394
Mohammad Musa <musa.mohammad.dev@gmail.com>
2026-08-21 12:44:12 +0530

    updates

--- hollowbutterfly.java
+++ hollowbutterfly.java
@@ -12,36 +12,36 @@ public class hollowbutterfly {
                         System.out.print(" ");
                     }
             }
-            for( int spaces = 1 ; spaces <= 2 * (n - i ); spaces++){
+            for( int spaces = 1 ; spaces <= 2 * (n - i); spaces++){
                 System.out.print(" ");
             }
             for ( int j = 1 ; j <= i ; j++){
- if( j == 1 || j== i ){
+             if( j == 1 || j== i ){
                 System.out.print("*");
                     } else { 
                         System.out.print(" ");
                     }
-    }
-    System.out.println();
-}
-for ( int i = n; i >= 1 ; i--){
+                }
+                System.out.println();
+            }
+                for ( int i = n; i >= 1 ; i--){
                 for ( int j = 1 ; j <= i ; j++){
- if( j == 1 || j== i ){ System.out.print("*");
+                if( j == 1 || j== i ){ System.out.print("*");
                 } else { 
                         System.out.print(" ");
                     }
-            }
-            for( int spaces = 1 ; spaces <= 2 * (n - i ); spaces++){
-                System.out.print(" ");
-            }
-            for ( int j = 1 ; j <= i ; j++){
- if( j == 1 || j== i ){
-                System.out.print("*");
-                    } else { 
+                }
+                        for( int spaces = 1 ; spaces <= 2 * (n - i ); spaces++){
                         System.out.print(" ");
                     }
+                        for ( int j = 1 ; j <= i ; j++){
+                         if( j == 1 || j== i ){
+                        System.out.print("*");
+                        } else { 
+                        System.out.print(" ");
+                    }
+                }
+                    System.out.println();
+            }
+        }
     }
-    System.out.println();
-}
-}
-}
