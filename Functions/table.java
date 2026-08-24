f873b9654c0d9e21705235c8363f1a8e2a6f5fa1
Mohammad Musa <musa.mohammad.dev@gmail.com>
2026-08-24 23:07:27 +0530

    updates

---
+++ Functions/table.java
@@ -0,0 +1,15 @@
+package Functions;
+import java.util.*;
+public class table {
+    public static void numberTable ( int n){
+        for ( int i = 1 ; i <= 10;i++){
+            System.out.println( n + " x " + i + " = " + (n * i) );
+
+        }
+    }
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            numberTable(n);
+}
+}
