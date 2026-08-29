fba05233e32e583db041b4f1da45a2cd0aa7aff0
Mohammad Musa <musa.mohammad.dev@gmail.com>
2026-08-29 17:17:34 +0530

    updates

--- Arrays/checkOrder.java
+++ Arrays/checkOrder.java
@@ -5,9 +5,23 @@ public class checkOrder {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
         int numbers [] = new int[size];
+
+
+
         for ( int i = 0 ; i < size ; i++){
             numbers [i] = sc.nextInt();
         }
-        
+        boolean asscending = true ;
+        for ( int i = 1 ; i < size ; i++){
+            if ( numbers [i] < numbers [i - 1]){
+                asscending = false;
+            }
+        }
+        if (asscending){
+            System.out.println("NUMBERS ARE IN ASSENDING ORDER");
+        } 
+        else { 
+            System.out.println(" THE NUMBERS ARE NOT IN ASSENDING ORDER ");
+        }
     }
 }
